10fb7ce8add97618062c3664fd1e60a649fc2046
nvasilakis <nikos.ailo@gmail.com>
2011-10-13 02:28:53 -0400

    Adding PATH dir addition/removal capabilities on generate script

--- generateTests.sh
+++ generateTests.sh
@@ -8,29 +8,78 @@
 #export PATH=/media/w7/Projects/blast-2.5_linux-bin-x86:$PATH;
 #export PATH=/media/w7/Projects/cvc-linux-1.0a/bin:$PATH;
 # need to log an aproximation profiling
+# -max-forks=1000 for KLEE
+
+# Adding export for KLEE in order to run
+# *.cde files only by invoking their name
+
+function configure_path {
+  klee_path="/media/w7/Projects/klee-cde-package/bin";
+  blast_path="/media/w7/Projects/blast-2.5_linux-bin-x86/";
+  cvc_path="/media/w7/Projects/cvc-linux-1.0a/";
+  # String comparison
+  if [[ $1 = "add" ]]; then
+    if [ -d $klee_path  ]; then
+      export PATH=$klee_path:$PATH;
+    else
+      echo "[ERROR] KLEE was not found"
+    fi
+    # Adding path for BLAST
+    if [ -d $blast_path  ]; then
+      export PATH=$blast_path:$PATH;
+    else
+      echo "[ERROR] BLAST was not found"
+    fi
+    # Adding path for cvc theorem
+    if [ -d  $cvc_path ]; then
+      export PATH=$cvc_path:$PATH;
+    else
+      echo "[ERROR] CVC Theorem Proover was not found"
+    fi
+  elif [[ $1 = "remove" ]]; then
+    PATH=$(echo $PATH | sed -e "s;:\?$klee_path;;")
+    PATH=$(echo $PATH | sed -e "s;:\?$blast_path;;")
+    PATH=$(echo $PATH | sed -e "s;:\?$cvc_path;;")
+    #In case we deleted something from the beginning
+    PATH=$(echo $PATH | sed -e "s;^:;;")
+  fi
+  echo -e "[Env] \$PATH=$PATH\n";
+}
 
 #if [[ ${file: -4} ~= /regex/ ]]
 prefix="out";
 results="$prefix/results.txt";
 cvc="$prefix/assertions.txt";
 
-
 if [ $# -eq 0 ]; then
   echo -e "\n You have not specified any source files
  That is $0 <file1.c> <file2.c>..\n";
 else
+
   for file in $*; do
+    #if [ `"$file" | grep -c '^\-\(s|setup\)' ` -eq 0 ]; then
     if [[ ! -f "$file" ]]; then
+      # Instead of boolean we compare with the number of occurences
+      if [ `echo "$file" | grep -c "^\-\(a\|add\)"` -eq 1 ]; then
+        configure_path 'add';
+        exit 0;
+      elif [ `echo "$file" | grep -c "^\-\(r\|remove\)"` -eq 1 ]; then
+        configure_path 'remove';
+        exit 0;
+      fi
       echo -e "File $file is not in `pwd` \n";
       exit 1;
     fi
   done
 fi
-
+configure_path 'add';
+echo $PATH
+# Add a -clean option
+echo -e "[Env] Recreating $results and $cvc folders\n";
 if [[ -f "$results" ]]; then
-  rm "$results";
+  rm "$results" "$cvc";
 fi
-touch "$results";
+touch "$results" "$cvc";
 
 for file in $*; do
   echo -e "\n[$file][Compiling to LLVM bitcode]\n";
@@ -39,7 +88,7 @@ for file in $*; do
   START=$(date +%s)
   klee.cde -write-cvcs `echo $file | sed "s/c$/o/"`;
   END=$(date +%s)
-# use grep to get the results
+  # use grep to get the results
   echo -e "\n[$file][Generated in $(( $END - $START )) seconds]\n";
   echo -e "\n[$file][Outputting results]\n";
   echo -e "\n[$file][Converting klee results from bin to ascii]\n" | tee -a "$results";
