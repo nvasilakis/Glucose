6b8466a6a922141acffd4f2d7f5b04b6ab74227b
nvasilakis <nikos.ailo@gmail.com>
2011-11-18 02:46:40 -0500

    Creating the diff class on report.py

--- report.py
+++ report.py
@@ -47,74 +47,91 @@ class Report():
     print "total paths:           " , total
   
 
-def getFiles(directory, extentionList = [".c"]):
-  """get assertions from multiple source files"""
-  fileList = [os.path.normcase(f) for f in os.listdir(directory)]
-  fileList = [os.path.join(directory, f) for f in fileList if os.path.splitext(f)[1] in extentionList ]
-  return [getAssertion(f) for f in fileList]
+class Introspect():
+  """given"""
+  def __init__(self, directory, extList= [".c"]):
+    self.directory = directory 
+    self.extList = extList
+      
+  def getFiles(self):
+    """get assertions from multiple source files"""
+    fileList = [os.path.normcase(f) for f in os.listdir(self.directory)]
+    fileList = [os.path.join(self.directory, f) for f in fileList if os.path.splitext(f)[1] in self.extList ]
+    return [self.getAssertion(f) for f in fileList]
 
-def getAssertion(filename):
-  """extract assertions from a file"""
-  try:
-    fsock = open(filename, "r", 0)
+  def getAssertion(self,f):
+    """extract assertions from a file"""
     try:
-      if ".c" in filename:
-        # hardcoded assertions
-        assertion=[]
-        assertion = [line[14:-2] for line in fsock if "#define STOP" in line]
-        return assertion[0]
-      else:
-        # dynamic assertions
-        assertion=[]
-        # simple checking -- use regex here
-        assertion = [line[1:-2] for line in fsock if "(" in line]
-        return assertion
-    finally:
-      fsock.close()
-  except IOError:
-    print "The file does not exist, exiting gracefully"
+      fsock = open(f, "r", 0)
+      try:
+        if ".c" in f:
+          # hardcoded assertions
+          assertion=[]
+          assertion = [line[14:-2] for line in fsock if "#define STOP" in line]
+          return assertion[0]
+        else:
+          # dynamic assertions
+          assertion=[]
+          # simple checking -- use regex here
+          assertion = [line[1:-2] for line in fsock if "(" in line]
+          return assertion
+      finally:
+        fsock.close()
+    except IOError:
+      print "The file does not exist, exiting gracefully"
 
-def getData(whereis, extentionList = [".c"]):
-  """fetch assertion data from a file or directory of files"""
-  assertions= []
-  if(whereis[-1]=="/" or whereis[-4]!="."):
-    assertions = getFiles(whereis, extentionList)
-  else:
-    assertions = getAssertion(whereis)
-  return assertions
+  def getData(self):
+    """fetch assertion data from a file or directory of files"""
+    assertions= []
+    if(self.directory[-1]=="/" or self.directory[-4]!="."):
+      assertions = self.getFiles()
+    else:
+      assertions = self.getAssertion(self.directory)
+    return assertions
 
-def diff(set1, set2):
-  """compares two assertion sets"""
-  common = ""
-  if set1.__len__() < set2.__len__():
-    common = set(set1).intersection(set2)
-  else:
-    common = set(set2).intersection(set1)
-  diff1 = set(set1) - common
-  diff2 = set(set2) - common
-  print "common:"
-  print common
-  print "differ:"
-  
-def print_to_file(set1, set2):
-  """print to file"""
-  fsock = open("diff.txt", "w", 0)
-  fsock.write("==hardcoded==")
-  for path in set1:
-    fsock.write(path+"\n")
-  fsock.write("==dynamic==")
-  for path in set2:
-    fsock.write(path+"\n")
-  fsock.close()
 
-def bylength(word1, word2):
-    """
-    write your own compare function:
-    returns value > 0 of word1 longer then word2
-    returns value = 0 if the same length
-    returns value < 0 of word2 longer than word1
-    """
-    return len(word1) - len(word2)
+
+class Diff():
+  """docstring for Diff"""
+  def __init__(self, file1, file2):
+    self.file1 = file1
+    self.file2 = file2
+    
+  def getDifference(self):
+    """compares two assertion sets"""
+    common = ""
+    if self.file1.__len__() < self.file2.__len__():
+      common = set(self.file1).intersection(self.file2)
+    else:
+      common = set(self.file2).intersection(self.file1)
+    diff1 = set(self.file1) - common
+    diff2 = set(self.file2) - common
+    print "common:"
+    print common
+    print "diff1:"
+    print diff1
+    print "diff2:"
+    print diff2
+    
+  def printToFile(self):
+    """print to file"""
+    fsock = open("diff.txt", "w", 0)
+    fsock.write("==hardcoded==")
+    for path in self.file1:
+      fsock.write(path+"\n")
+    fsock.write("==dynamic==")
+    for path in self.file2:
+      fsock.write(path+"\n")
+    fsock.close()
+
+  def bylength(word1, word2):
+      """
+      write your own compare function:
+      returns value > 0 of word1 longer then word2
+      returns value = 0 if the same length
+      returns value < 0 of word2 longer than word1
+      """
+      return len(word1) - len(word2)
 
 if __name__=="__main__":
   #hardcoded = getData("/media/w7/Projects/UPenn/software-engineering/Glucose/hardcoded/", [".c"])
